253edc12643f3d1bdeb43c096ffd429d7e544f03
Hugo Reis <hugo.carvalho.reis@gmail.com>
2020-03-27 20:45:13 -0300

    Added front-end of application

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FaCloud } from 'react-icons/fa';
+
+import api from '../../services/api';
+
+import './styles.css';
+// import herosImg from '../../assets'
+// import logoImg from '../../assets'
+
+export default function Logon() {
+
+    const [id, setiD] = useState('');
+
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try {
+            
+            const response = await api.post('sessions', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+
+        } catch (error) {
+            alert('Falha no login. Tente novamente');
+        }
+    }
+
+    return (
+        <div className="logon-container">
+            <section className="form">
+            <img src="" alt="Be The Hero" />
+            {/* logoImg */}
+
+            <form onSubmit={handleLogin}>
+                <h1>Faça seu logon</h1>
+
+                <input 
+                    placeholder="Sua ID"
+                    value={id}
+                    onChange={e => setiD(e.target.value)} />
+                <button className="button" type="submit">Entrar</button>
+
+                <Link className="back-link" to="/register">
+                    <FaCloud size={16} color="#E02041"/>
+                    Não tenho cadastro
+                </Link>
+            </form>
+
+            </section>
+
+            <img src="" alt="Heroes" /> 
+            {/* herosImg */}
+        </div>
+    );
+}
